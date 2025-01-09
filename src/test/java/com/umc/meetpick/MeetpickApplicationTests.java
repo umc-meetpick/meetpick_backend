bda2369dfa5d0d7bb6f1bc1e913d8b0bf2a66119
YamYamee <cartcnx@naver.com>
2025-01-09 23:11:01 +0900

    inital setting

---
+++ src/test/java/com/umc/meetpick/MeetpickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.meetpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetpickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
