8199853d63ebc617185c5fb34851ae5ccda7dae6
지연 <158806908+Heize-jiyean@users.noreply.github.com>
2025-01-21 13:17:17 +0900

    Feat: 매칭 리스트 API 구현 (#14)

--- src/main/java/com/umc/meetpick/repository/NewRequestRepository.java
+++ src/main/java/com/umc/meetpick/repository/NewRequestRepository.java
@@ -4,6 +4,7 @@ import com.umc.meetpick.entity.Request;
 import com.umc.meetpick.enums.MateType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.umc.meetpick.enums.MateType;  // MateType import 추가
 
 // 새로운 매칭 요청 api 관련 Repository
 @Repository
