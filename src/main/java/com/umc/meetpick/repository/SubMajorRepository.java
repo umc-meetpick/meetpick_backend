de89bbf3215e61452c156bf6d6c04871ea090499
YamYamee <cartcnx@naver.com>
2025-02-17 12:36:53 +0900

    전공 정보 반환하게 수정

--- src/main/java/com/umc/meetpick/repository/SubMajorRepository.java
+++ src/main/java/com/umc/meetpick/repository/SubMajorRepository.java
@@ -1,9 +1,12 @@
 package com.umc.meetpick.repository;
 
+import com.umc.meetpick.entity.Major;
 import com.umc.meetpick.entity.SubMajor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +15,6 @@ public interface SubMajorRepository extends JpaRepository<SubMajor, Long> {
 
     // TODO 나중에 지울 예정
     SubMajor findByNameOrderByName(String name);
+
+    List<SubMajor> findAllByMajor(@Param("major") Major major);
 }
