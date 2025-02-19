6a4f37dd6c92faa77d859e573f64bf0b7bbd7145
YamYamee <cartcnx@naver.com>
2025-02-19 17:09:04 +0900

    메소드 한개에 적용

--- src/main/java/com/umc/meetpick/common/aspect/ExecutionTimeAspect.java
+++ src/main/java/com/umc/meetpick/common/aspect/ExecutionTimeAspect.java
@@ -4,11 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Slf4j
 @Component
+@Order(0)
 public class ExecutionTimeAspect {
 
     @Around("@annotation(com.umc.meetpick.common.annotation.TrackExecutionTime)" +
--- src/main/java/com/umc/meetpick/service/home/HomeServiceImpl.java
+++ src/main/java/com/umc/meetpick/service/home/HomeServiceImpl.java
@@ -9,6 +9,7 @@ import com.umc.meetpick.repository.member.MemberSecondProfileRepository;
 import com.umc.meetpick.service.home.factory.MemberQueryStrategyFactory;
 import com.umc.meetpick.service.home.strategy.MemberQueryStrategy;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,6 +25,7 @@ public class HomeServiceImpl implements HomeService {
 
     // TODO 레디스 사용하기, 무작위 멤버 추출 방식 바꾸기
     @TrackExecutionTime
+    @Cacheable(cacheManager = "GeneralCacheManager", key = "#p0", value = "RandomCache")
     public MemberResponseDTO getRandomMember(String mateType){
 
         MateType type = MateType.fromString(mateType);
