69e81cf55aa21e863be9821ad699d8093edd8f49
YamYamee <82762402+YamYamee@users.noreply.github.com>
2025-02-19 15:50:53 +0900

    add: 신청 조건 임시로 추가 (#152)

--- src/main/java/com/umc/meetpick/service/request/RequestServiceImpl.java
+++ src/main/java/com/umc/meetpick/service/request/RequestServiceImpl.java
@@ -1,5 +1,7 @@
 package com.umc.meetpick.service.request;
 
+import com.umc.meetpick.common.exception.handler.GeneralHandler;
+import com.umc.meetpick.common.response.status.ErrorCode;
 import com.umc.meetpick.dto.MatchResponseDto;
 import com.umc.meetpick.dto.RequestDTO;
 import com.umc.meetpick.entity.*;
@@ -230,42 +232,40 @@ public class RequestServiceImpl implements RequestService {
             throw new IllegalArgumentException("본인 매칭에 신청 불가");
         }
 
-//        // 조건에 맞는지 판단 - 성별
-//        if(!(request.getGender() == null) && joinMember.getGender() != request.getGender()){
-//            throw new IllegalArgumentException("성별 조건 안 맞음");
-//        }
-//
-//        // 조건에 맞는지 판단 - 나이 범위
-//        if(!(request.getMinAge() == null)){
-//            if (request.getMinAge() > joinMember.getAge() || request.getMaxAge() < joinMember.getAge()) {
-//                throw new IllegalArgumentException("나이 조건 안 맞음");
-//            }
-//        }
+        // 조건에 맞는지 판단 - 성별
+        if(!(request.getGender() == null) && joinMember.getGender() != request.getGender()){
+            throw new GeneralHandler(ErrorCode.GENDER_NOT_SATISFIED);
+        }
 
-//        // 조건에 맞는지 판단 - 학번
-//        if(request.getStudentNumber() == PEER){
-//            if (!(joinMemberProfile.getStudentNumber() == requestOwnerMemberProfile.getStudentNumber())){
-//                throw new IllegalArgumentException("학번 조건 안 맞음");
-//            }
-//        } else if(request.getStudentNumber() == SENIOR ){
-//            if (!(joinMemberProfile.getStudentNumber() < requestOwnerMemberProfile.getStudentNumber())){
-//                throw new IllegalArgumentException("학번 조건 안 맞음");
-//            }
-//        } else if(request.getStudentNumber() == JUNIOR){
-//            if (!(joinMemberProfile.getStudentNumber() > requestOwnerMemberProfile.getStudentNumber())){
-//                throw new IllegalArgumentException("학번 조건 안 맞음");
-//            }
-//        }
+        // 조건에 맞는지 판단 - 나이 범위
+        if(!(request.getMinAge() == null)){
+            if (request.getMinAge() > joinMember.getAge() || request.getMaxAge() < joinMember.getAge()) {
+                throw new GeneralHandler(ErrorCode.GENDER_NOT_SATISFIED);
+            }
+        }
 
-        // 조건에 맞는지 판단 - submajor - 나중에
+        // 조건에 맞는지 판단 - 학번
+        if(request.getStudentNumber() == PEER){
+            if (!(joinMemberProfile.getStudentNumber() == requestOwnerMemberProfile.getStudentNumber())){
+                throw new GeneralHandler(ErrorCode.STUDENT_NUMBER_NOT_SATISFIED);
+            }
+        } else if(request.getStudentNumber() == SENIOR ){
+            if (!(joinMemberProfile.getStudentNumber() < requestOwnerMemberProfile.getStudentNumber())){
+                throw new GeneralHandler(ErrorCode.STUDENT_NUMBER_NOT_SATISFIED);
+            }
+        } else if(request.getStudentNumber() == JUNIOR){
+            if (!(joinMemberProfile.getStudentNumber() > requestOwnerMemberProfile.getStudentNumber())){
+                throw new GeneralHandler(ErrorCode.STUDENT_NUMBER_NOT_SATISFIED);
+            }
+        }
 
-//        // 취미 동일 여부 - 하나라도 같으면 통과
-//        if(request.getIsHobbySame()){
-//            if(!(Collections.disjoint(joinMemberProfile.getHobbies(),requestOwnerMemberProfile.getHobbies()))){
-//                throw new IllegalArgumentException("취미 조건 안 맞음");
-//            }
-//        }
-//
+        // 취미 동일 여부 - 하나라도 같으면 통과
+        if(request.getIsHobbySame()){
+            if(!(Collections.disjoint(joinMemberProfile.getHobbies(),requestOwnerMemberProfile.getHobbies()))){
+                throw new IllegalArgumentException("취미 조건 안 맞음");
+            }
+        }
+
 //        // 성격 판단 - 입력받은 mbti 가져와서 판단
 //        String joinMemberMBTI = joinMemberProfile.getMBTI().name();
 //        String requestMBTI = request.getMbti();
