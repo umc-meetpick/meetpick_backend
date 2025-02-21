9fdf5106b312c0c7d82f29148cd4e5c7bc49a0e2
YamYamee <82762402+YamYamee@users.noreply.github.com>
2025-02-21 10:47:00 +0900

    url 변경 (#211)

--- src/main/java/com/umc/meetpick/common/config/SecurityConfig.java
+++ src/main/java/com/umc/meetpick/common/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOriginPatterns(List.of("https://*.meetpick.click", "http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type")); // 허용할 헤더
         configuration.setAllowCredentials(true); // 인증 정보 포함 허용 (JWT 사용 시 필요)
